47389718ee6fea8ef6e20c1d9ae0df55d8181814
PrajwalNiraml7 <prajwalnirmal8@gmail.com>
2024-12-01 00:29:28 +0530

    DotNet Assignment Question 10 Completed

---
+++ Question_10/Manager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeelib
+{
+    public class Manager : Employee
+    {
+		private double _Bonus;
+
+		public double Bonus
+		{
+			get { return _Bonus; }
+			set { _Bonus = value; }
+		}
+        public Manager()
+        {
+            
+        }
+
+        public Manager(string name, char gender, Date birth, string address, double salary, string designation, DepartmentType dept , double bonus) : base(name, gender, birth, address, salary, "Manager", dept)
+        {
+            
+            _Bonus = bonus;
+        }
+
+        public override void AcceptDetails()
+        {
+            base.AcceptDetails();
+            Console.WriteLine("Enter the Bonus");
+            _Bonus = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override void PrintDetails()
+        {
+            base.PrintDetails();
+            Console.WriteLine("Bonus : "+Bonus);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Bonus : "+Bonus+" .";
+        }
+
+
+
+    }
+}
