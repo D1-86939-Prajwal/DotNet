740523289ab183dc7da92622c8207c6ef5c24066
PrajwalNiraml7 <prajwalnirmal8@gmail.com>
2024-12-01 00:29:51 +0530

    DotNet Assignment Question 13 Completed

---
+++ Question_13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employeelib;
+
+namespace Question13
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            company.AcceptDetails();
+            company.PrintDetails();
+            company.AddEmployee();
+            company.AddEmployee();
+            company.PrintDetails();
+            company.PrintEmployees();
+            Console.WriteLine("Enter the id to search : ");
+            int key = Convert.ToInt16(Console.ReadLine());
+            company.FindEmployee(key);
+            Console.WriteLine(company.CalculateSalaryExpense());
+
+
+
+        }
+    }
+}
