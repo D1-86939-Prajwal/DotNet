7a610e5d6ccff398e29ce21f0305b49476e800d2
PrajwalNiraml7 <prajwalnirmal8@gmail.com>
2024-12-01 00:29:58 +0530

    DotNet Assignment Question 14 Completed

---
+++ Question_14/Company.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employeelib;
+
+namespace Question14
+{
+    public class Company
+    {
+        private string _Cname;
+        private string _Address;
+        private double _SalaryExpense;
+        private LinkedList<Employee> _EmployeeList;
+
+        public double SalaryExpense
+        {
+            get { return _SalaryExpense; }
+            set { _SalaryExpense = value; }
+        }
+
+
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+
+
+        public string Cname
+        {
+            get { return _Cname; }
+            set { _Cname = value; }
+        }
+
+        public Company()
+        {
+            _Cname = "Cure All";
+            _Address = "Pune";
+            _EmployeeList = new LinkedList<Employee>();
+        }
+
+        public Company(string cname, string address)
+        {
+            _Cname = cname;
+            _Address = address;
+            _EmployeeList = new LinkedList<Employee>();
+        }
+
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter the name of Company : ");
+            _Cname = Console.ReadLine();
+
+            Console.WriteLine("Enter the address of Company : ");
+            _Address = Console.ReadLine();
+
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("Company Name : " + _Cname);
+            Console.WriteLine("Address : " + _Address);
+        }
+
+        public double CalculateSalaryExpense()
+        {
+            _SalaryExpense = 0;
+            foreach (Employee emp in _EmployeeList)
+            {
+                _SalaryExpense = _SalaryExpense + emp.Salary;
+            }
+
+            return _SalaryExpense;
+
+        }
+
+        public void AddEmployee()
+        {
+            Employee emp = new Employee();
+            emp.AcceptDetails();
+            _EmployeeList.AddFirst(emp);
+        }
+        public void RemoveEmployee(int id)
+        {
+            foreach (Employee emp in _EmployeeList)
+            {
+                if (emp.Id == id)
+                {
+                    _EmployeeList.Remove(emp);
+                }
+            }
+        }
+
+        public Employee FindEmployee(int id)
+        {
+            foreach (Employee emp in _EmployeeList)
+            {
+                if (emp.Id == id)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Cname : " + Cname + " Address : " + Address + " .";
+        }
+
+        public void PrintEmployees()
+        {
+            foreach (Employee emp in _EmployeeList)
+            {
+                Console.WriteLine("-----------------------------");
+                emp.PrintDetails();
+                Console.WriteLine("-----------------------------");
+            }
+        }
+    }
+}
---
+++ Question_14/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question14
+{
+    internal class Program
+    {
+        public static int menu()
+        {
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Accept Company Details");
+            Console.WriteLine("2. Add the Employee");
+            Console.WriteLine("3. Remove the Employee");
+            Console.WriteLine("4. Find the Employee");
+            Console.WriteLine("5. Display Company Info");
+            Console.WriteLine("6. Display all Employees");
+            Console.WriteLine("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            return choice;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+
+            int choice;
+            while ((choice = menu()) != 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("-----------------------------");
+                        company.AcceptDetails();
+                        Console.WriteLine("-----------------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("-----------------------------");
+                        company.AddEmployee();
+                        Console.WriteLine("-----------------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the Id to remove employee : ");
+                        int key = Convert.ToInt32(Console.ReadLine());
+                        company.RemoveEmployee(key);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the id to find the employee : ");
+                        int key1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("-----------------------------");
+                        company.FindEmployee(key1).PrintDetails();
+                        Console.WriteLine("-----------------------------");
+                        break;
+                    case 5:
+                        Console.WriteLine("-----------------------------");
+                        company.PrintDetails();
+                        Console.WriteLine("-----------------------------");
+                        break;
+                    case 6:
+                        Console.WriteLine("-----------------------------");
+                        company.PrintEmployees();
+                        Console.WriteLine("-----------------------------");
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid choice !!!");
+                        break ;
+                }
+            }
+        }
+    }
+}
